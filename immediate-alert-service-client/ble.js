9a2d55644984e65db3c2b151165d59e6689f57f8
Lizzie Prader <lizzie@moddable.tech>
2020-08-28 15:57:59 -0700

    ias: only show popup when necessary

--- immediate-alert-service-client/ble.js
+++ immediate-alert-service-client/ble.js
@@ -16,47 +16,57 @@ const alertCharacteristicUuid = 0x2A06;
 let alertCharacteristic;
 
 function onAlertButtonClick() {
-	log('Requesting Bluetooth Device...');
-	navigator.bluetooth.requestDevice({ filters: [{services: [serviceUuid]}]})
-	.then(device => {
-		log('Connecting to GATT Server...');
-		return device.gatt.connect();
-	})
-  	.then(server => {
-		log('Getting Service...');
-		return server.getPrimaryService(serviceUuid);
-  	})
-  	.then(service => {
-		log('Getting Characteristics...');
-		return service.getCharacteristics();
-  	})
-	.then(characteristics => {
-		characteristics.forEach(characteristic => {
-			if (characteristic.uuid.indexOf(alertCharacteristicUuid.toString(16)) > -1) {
-				alertCharacteristic = characteristic;
-				document.getElementById("alert").disabled = true;
-				document.getElementById("found").disabled = false;
-				let alert = Uint8Array.of(1);
-				alertCharacteristic.writeValue(alert)
-				.catch(error => {
-					log('Error! ' + error);
-				});
-			}
-			else {
-				characteristic.startNotifications()
-				.then(_ => {
-					log('> Notifications started');
-					characteristic.addEventListener('characteristicvaluechanged', handleNotifications);
-				})
-				.catch(error => {
-					log('Error! ' + error);
-				});
-			}
+	if (undefined === alertCharacteristic) {	
+		log('Requesting Bluetooth Device...');
+		navigator.bluetooth.requestDevice({ filters: [{services: [serviceUuid]}]})
+		.then(device => {
+			log('Connecting to GATT Server...');
+			return device.gatt.connect();
+		})
+	  	.then(server => {
+			log('Getting Service...');
+			return server.getPrimaryService(serviceUuid);
+	  	})
+	  	.then(service => {
+			log('Getting Characteristics...');
+			return service.getCharacteristics();
+	  	})
+		.then(characteristics => {
+			characteristics.forEach(characteristic => {
+				if (characteristic.uuid.indexOf(alertCharacteristicUuid.toString(16)) > -1) {
+					alertCharacteristic = characteristic;
+					document.getElementById("alert").disabled = true;
+					document.getElementById("found").disabled = false;
+					let alert = Uint8Array.of(1);
+					alertCharacteristic.writeValue(alert)
+					.catch(error => {
+						log('Error! ' + error);
+					});
+				}
+				else {
+					characteristic.startNotifications()
+					.then(_ => {
+						log('> Notifications started');
+						characteristic.addEventListener('characteristicvaluechanged', handleNotifications);
+					})
+					.catch(error => {
+						log('Error! ' + error);
+					});
+				}
+			});
+		})
+		.catch(error => {
+			log('Error! ' + error);
 		});
-	})
-	.catch(error => {
-		log('Error! ' + error);
-	});
+	} else {
+		document.getElementById("alert").disabled = true;
+		document.getElementById("found").disabled = false;
+		let alert = Uint8Array.of(1);
+		alertCharacteristic.writeValue(alert)
+		.catch(error => {
+			log('Error! ' + error);
+		});		
+	}
 }
 
 function onFoundButtonClick() {
